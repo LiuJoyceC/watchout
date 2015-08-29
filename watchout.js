d7c95feb0bdf7a59c39d1ab4897123b5c28c24b8
Joyce Liu <liujoycec@gmail.com>
2015-08-29 16:33:15 -0700

    Changed theme of game to mario kart. Turned enemies into koopa shells, players into mario kart characters, changed backgrounds and player colors, and made minor adjustments to player sizes and the max and min positions to keep them inside the gameplay arena.

--- watchout.js
+++ watchout.js
@@ -5,18 +5,20 @@ do{
 
 var gameOptions = {
   width: 1000,
-  height: 500,
-  numEnemies: 25,
-  speed: 1000
+  height: 600,
+  numEnemies: 20,
+  speed: 1250
 };
 
 var gameboard = d3.select('.container').append('svg').attr('width', gameOptions.width)
   .attr('height', gameOptions.height).classed('gameboard',true);
 
-var Player = function(x, y, color){
+var Player = function(x, y, image, color){
   this.x = x;
   this.y = y;
   this.color = color;
+  this.image = image;
+  this.imageURL = image + '.gif';
   this.score = 0;
   this.highScore = 0;
   this.increaseScore();
@@ -27,28 +29,32 @@ Player.prototype.increaseScore = function(){
   this.highScore = Math.max(this.score, this.highScore);
   setTimeout(this.increaseScore.bind(this), 1000);
 };
-var colors = ['red', 'blue', 'orange', 'green'];
+var playerImages = ['mario', 'yoshi', 'bowser', 'peach'];
+var colors =['#F8020F','#00F800','#FFB500','#F8B0B0'];
 var players = [];
 for(var i = 0; i < playerNum; i++){
-  players[i] = new Player(((i + 1) * gameOptions.width)/(playerNum + 1), gameOptions.height/2, colors[i]);
+  players[i] = new Player(((i + 1) * gameOptions.width)/(playerNum + 1), gameOptions.height/2, playerImages[i], colors[i]);
 }
 
-var playerCircles = gameboard.selectAll()
+var playerCircles = gameboard.selectAll('.player')
   .data(players)
   .enter()
-  .append('circle')
+  .append('image')
   .attr({
-    'cx': function(d){return d.x;},
-    'cy': function(d){return d.y;},
-    'r': 10,
-    'id': function(d){return d.color;}
-  })
-  .style('fill', function(d){return d.color;});
+    'xlink:href': function(d) {return d.imageURL;},
+    'class': 'player',
+    'x': function(d){return d.x;},
+    'y': function(d){return d.y;},
+    'height': 35,
+    'width': 35,
+    'r': 20,
+    'id': function(d){return d.image;}
+  });
 
 var drag = d3.behavior.drag().on('drag',function() {
   d3.select(this).attr({
-    'cx': Math.min(Math.max(d3.event.x, 10), gameOptions.width - 10),
-    'cy': Math.min(Math.max(10, d3.event.y), gameOptions.height - 10)
+    'x': Math.min(Math.max(d3.event.x, 0), gameOptions.width - 35),
+    'y': Math.min(Math.max(0, d3.event.y), gameOptions.height - 35)
   });
 });
 
@@ -75,7 +81,7 @@ var enemyGenerator = function() {
   var enemies = [];
   for (var i = 0; i < gameOptions.numEnemies; i++) {
     var enemy = {
-      r:10,
+      r: 10,
       x: randomX(),
       y: randomY(),
       index: 'ind' + i
@@ -94,11 +100,13 @@ var randomY = function() {
 };
 
 var enemies = enemyGenerator();
-var enemySquares = gameboard.selectAll('rect')
+var enemySquares = gameboard.selectAll('.koopa')
   .data(enemies)
   .enter()
-  .append('rect')
+  .append('image')
   .attr({
+    'xlink:href': 'koopa shell.png',
+    'class': 'koopa',
     'x': function(d) {return d.x;},
     'y': function(d) {return d.y;},
     'width': function(d) {return 2*d.r;},
@@ -108,11 +116,11 @@ var enemySquares = gameboard.selectAll('rect')
   .style('fill', 'purple');
 
 var checkCollision = function() {
-  d3.selectAll('circle').each(function(d) {
+  d3.selectAll('.player').each(function(d) {
     var totalRadius = 20;
-    var playerX = d3.select('#' + d.color).attr('cx');
-    var playerY = d3.select('#' + d.color).attr('cy');
-    d3.selectAll('rect').each(function(enemy) {
+    var playerX = d3.select('#' + d.image).attr('x');
+    var playerY = d3.select('#' + d.image).attr('y');
+    d3.selectAll('.koopa').each(function(enemy) {
       var dx = d3.select('#' + enemy.index).attr('x') - playerX;
       var dy = d3.select('#' + enemy.index).attr('y') - playerY;
       if (Math.sqrt(dx * dx + dy * dy)  < totalRadius) {
